522bc9aa4506d0440ea700849faf7c9e41e4ef71
Asus\A s u s <masood_a_t@yahoo.com>
2019-05-21 01:15:48 +0430

    waitingroom add player with no limit

--- Assets/WaitingRoomCode.cs
+++ Assets/WaitingRoomCode.cs
@@ -18,12 +18,73 @@ public class WaitingRoomCode : MonoBehaviour
         public Text IsMaster;
         public Button Buttonready;
         public Text StatePlayer;
+        public GameObject obj;
+        public bool Isready;
+
+        public PlayerClass(GameObject _obj, GameObject root, int row)
+        {
+            obj = _obj;
+            PlayerName = obj.transform.GetChild(0).GetComponent<Text>();
+            IsMaster = obj.transform.GetChild(1).GetComponent<Text>();
+            Buttonready = obj.transform.GetChild(2).GetComponent<Button>();
+            StatePlayer = obj.transform.GetChild(3).GetComponent<Text>();
+            obj.transform.SetParent(root.transform, false);
+            obj.transform.position = root.transform.position + new Vector3(0, row * 100, 0);
+        }
+
+        public void SetFields(int playerid)
+        {
+            PlayerName.text = PhotonNetwork.PlayerList[playerid].NickName;
+            name = PhotonNetwork.PlayerList[playerid].NickName;
+            IsMaster.text = (PhotonNetwork.PlayerList[playerid].IsMasterClient) ? "[Master]" : "[Client]";
+
+            object Pready;
+            if(PhotonNetwork.PlayerList[playerid].CustomProperties.TryGetValue(luncher.PLAYER_READY, out Pready))
+            {
+                Isready = (bool)Pready;
+                if (PhotonNetwork.NickName == PhotonNetwork.PlayerList[playerid].NickName)
+                {
+                    Buttonready.gameObject.SetActive(!(bool)Pready);
+                    StatePlayer.gameObject.SetActive((bool)Pready);
+                    StatePlayer.text= (!(bool)Pready) ? "Waiting ...": "Ready";
+                  
+                }
+                else
+                {
+                    Buttonready.gameObject.SetActive(false);
+                    Buttonready.gameObject.SetActive(true);
+                    //if (!(bool)Pready) players[i].StatePlayer.text = "Waiting ...";
+                    //else players[i].StatePlayer.text = "Ready";
+                    StatePlayer.text= (!(bool)Pready)? "Waiting ...":"Ready"; 
+                }
+                    
+            }
+            else
+            {
+                Isready = false;
+                StatePlayer.text = "Waiting ...";
+                if (PhotonNetwork.NickName == PhotonNetwork.PlayerList[playerid].NickName)
+                {
+                    Buttonready.gameObject.SetActive(true);
+                    StatePlayer.gameObject.SetActive(false);
+                }
+                else
+                {
+                    Buttonready.gameObject.SetActive(false);
+                    StatePlayer.gameObject.SetActive(true);
+                }
+            }
+
+        }
 
     }
 
-    public PlayerClass[] players = new PlayerClass[2];
+    public List<PlayerClass> players = new List<PlayerClass>();
     public Button StartGame;
     public luncher luncher;
+    public GameObject WaitRoomPlayeDataPrefab;
+    public GameObject ContentRoot;
+    public RectTransform contentRectTransform;
 
     // Use this for initialization
     void Start()
@@ -45,17 +106,23 @@ public class WaitingRoomCode : MonoBehaviour
     }
     public void ClearData()
     {
-        for (int i = 0; i < 2; i++)
-        {
-            players[i].PlayerName.text = "Waiting....";
-            players[i].IsMaster.text = "Waiting....";
-            players[i].Buttonready.gameObject.SetActive(false);
-            players[i].StatePlayer.gameObject.SetActive(false);
+        //for (int i = 0; i < 2; i++)
+        //{
+        //    players[i].PlayerName.text = "Waiting....";
+        //    players[i].IsMaster.text = "Waiting....";
+        //    players[i].Buttonready.gameObject.SetActive(false);
+        //    players[i].StatePlayer.gameObject.SetActive(false);
 
+        //}
+        for (int i = 0; i < players.Count; i++)
+        {
+            Destroy(players[i].obj);
+            //Destroy(players[i]);
         }
+        players.Clear();
         StartGame.gameObject.SetActive(false);
     }
-    public void refresh()
+    public void refresh0()
     {
         ClearData();
         bool readystate = true;
@@ -116,6 +183,23 @@ public class WaitingRoomCode : MonoBehaviour
         }
 
 
+
+        StartGame.gameObject.SetActive((PhotonNetwork.PlayerList.Length > 1) && readystate && (PhotonNetwork.IsMasterClient));
+    }
+
+    public void refresh()
+    {
+        ClearData();
+        players = new List<PlayerClass>();
+        bool readystate = true;
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        {
+            GameObject go = Instantiate(WaitRoomPlayeDataPrefab);
+            PlayerClass temp = new PlayerClass(go, ContentRoot, i);
+            temp.SetFields(i);
+            players.Add(temp);
+            readystate = readystate && temp.Isready;
+        }
 
         StartGame.gameObject.SetActive((PhotonNetwork.PlayerList.Length > 1) && readystate && (PhotonNetwork.IsMasterClient));
     }
